790a2cad798367a7a7c83a0e99aa68fa5ebf2901
Charalampos Gkikas <gkikas@idika.gr>
2018-02-01 23:22:40 +0200

    tendo and fire addition to the script

--- delete_content.py
+++ delete_content.py
@@ -1,5 +1,7 @@
 import datetime
+from tendo import singleton
 import praw
+import fire
 
 def get_date(reddit_object):
     time = reddit_object.created
@@ -13,9 +15,30 @@ def get_all_queries(sublisting):
     return [sublisting.controversial, sublisting.hot,
             sublisting.new, sublisting.top]
 
-def main():
+def sane_arguments(fire_input):
+    """Transform the argument into a list of strings"""
+    type_of = type(fire_input).__name__
+    result = []
+
+    # Try to convert possible
+    if ((type_of == 'str') or (type_of == 'int') or (type_of == 'float')):
+        result = [fire_input]
+    elif type_of == 'list':
+        result = fire_input[:]
+    elif type_of == 'tuple':
+        result = list(fire_input)
+    elif type_of == 'dict':
+        result = [key for key in fire_input]
+
+    result = [i if type(i).__name__ == 'str' else str(i) for i in result]
+    return result
+
+def delete_content(subreddits, days_old):
     deleted_comments = 0
     deleted_submissions = 0
+
+    subreddits_permitted = sane_arguments(subreddits)
+
     # Create the Reddit Instance based on the configuration (praw.ini)
     reddit = praw.Reddit("deletebot")
     me_redditor = reddit.user.me()
@@ -23,20 +46,26 @@ def main():
     # Quering for comments
     for query in get_all_queries(me_redditor.comments):
         for comment in query(limit=None):
-            if ((comment.subreddit == 'greece')
-                    and (is_days_old(get_date(comment), 365))):
+            if ((comment.subreddit in subreddits_permitted)
+                    and (is_days_old(get_date(comment), days_old))):
+                comment.delete()
                 deleted_comments += 1
     print("Comments deleted: %d" % (deleted_comments))
 
     # Quering for submissions
     for query in get_all_queries(me_redditor.submissions):
         for submission in query(limit=None):
-            if ((submission.subreddit == 'greece')
-                    and (is_days_old(get_date(submission), 365))):
+            if ((submission.subreddit in subreddits_permitted)
+                    and (is_days_old(get_date(submission), days_old))):
+                submission.delete()
                 deleted_submissions += 1
     print("Submissions deleted: %d" % (deleted_submissions))
 
-
+def main():
+    """The main function of the script. It prohibits to run in parallel
+       and also make the argument parsing"""
+    _ = singleton.SingleInstance()
+    fire.Fire(delete_content)
 
 if __name__ == '__main__':
     main()
